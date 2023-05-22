09893a1c8a6abf709e0970c04a2f9b2f29359be6
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-22 12:10:56 -0400

    set up Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
+import Screen2 from './src/screens/Screen2';
 
 export type RootStackParamList = {
-  Home: undefined
-}
+  Home: undefined;
+  Screen2: undefined;
+};
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): JSX.Element {
- 
-
   return (
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 
 export default App;
