4b1fac0da30739212e2ddccd2bc31bec067a2894
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-22 12:00:30 -0400

    created second screen

---
+++ src/screens/Screen2.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Screen2 = (props: Props) => {
+  return (
+    <View>
+      <Text>Screen2</Text>
+    </View>
+  );
+};
+
+export default Screen2;
