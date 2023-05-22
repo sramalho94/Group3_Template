1251207771e26e3d0649ba0fbc94157cc38bf42c
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-22 11:53:19 -0400

    created TestComponent

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const TestComponent = (props: Props) => {
+  return (
+    <View>
+      <Text>TestComponent</Text>
+    </View>
+  );
+};
+
+export default TestComponent;
