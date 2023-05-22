0572ba246fe2e3fcfae1c399d722c357a6b61e82
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-22 12:02:56 -0400

    react navigation configuration for ios and android

--- android/app/src/main/java/com/group3_template/MainActivity.java
+++ android/app/src/main/java/com/group3_template/MainActivity.java
@@ -1,5 +1,6 @@
 package com.group3_template;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Group3_Template";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
